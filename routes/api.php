c6d72192edded870fc0beb2287f261da406ad39c
Derian Pinto <pintoderian@hotmail.com>
2024-11-08 16:56:25 -0500

    Create files for the api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers\Api\CourseController;
+use App\Http\Controllers\Api\CommentController;
+use App\Http\Controllers\Api\ProgressController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +21,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->group(function () {
+    // Listar cursos
+    Route::get('courses', [CourseController::class, 'index']);
+
+    // Buscar cursos
+    Route::get('courses/search', [CourseController::class, 'search']);
+
+    // Registrar usuario en curso
+    Route::post('courses/{course}/register', [CourseController::class, 'registerUser']);
+
+    // Ver videos del curso
+    Route::get('courses/{course}/videos', [CourseController::class, 'videos']);
+
+    // Subir comentarios
+    Route::post('comments', [CommentController::class, 'store']);
+
+    // Dar likes
+    Route::post('comments/{comment}/like', [CommentController::class, 'like']);
+
+    // Manejar progreso
+    Route::post('progress/{course}/update', [ProgressController::class, 'update']);
 });
